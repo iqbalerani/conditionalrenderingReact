ec3fee0f8108d8d24d772cc668a43eaf8d40958f
Iqbal Erani <iqbalerani@gmail.com>
2023-02-24 18:20:07 +0500

    2 commit

--- src/Joke.js
+++ src/Joke.js
@@ -2,10 +2,6 @@ import React from "react";
 
 export default function Joke(props) {
   const [isShown, setIsShown] = React.useState(false);
-  /**
-   * Challenge:
-   * - Only display the punchline paragraph if `isShown` is true
-   */
   function toggleShown() {
     setIsShown((prevShown) => !prevShown);
   }
@@ -13,14 +9,10 @@ export default function Joke(props) {
     <div>
       {props.setup && <h3>{props.setup}</h3>}
       {isShown && <p>{props.punchline}</p>}
-      <button onClick={toggleShown}>Show Punchline</button>
+      <button onClick={toggleShown}>
+        {isShown ? "Hide" : "Show"} Punchline
+      </button>
       <hr />
     </div>
   );
-}
-
-const cond1 = false;
-const cond2 = false;
-if (cond1 && console.log("Hello there")) {
-  // this code will NOT run
 }
